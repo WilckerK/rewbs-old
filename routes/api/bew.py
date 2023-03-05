6765d98e3aab300ccfbf0af5c26b732e66a343b4
Wilcker Kauã <85180287+WilckerK@users.noreply.github.com>
2023-03-05 15:38:31 -0300

    Update bew.py

--- routes/api/bew.py
+++ routes/api/bew.py
@@ -2,8 +2,19 @@ from __main__ import api, db
 from flask import render_template
 
 @api.route('/bew/<string:id>')
-def bews(id):
-    print(id)
-    user = db.users.find_one_or_404({ "discordId": id })
+def query_bew_id(id):
+	# data = request.json # Get request
+	# print(data)
 
-    return render_template('bews.html', bews=user['bews'])
+	# Check if have necessary values
+	# if sorted(list(data.keys()))  != ['bewId']:
+	# 	return response_code('400')
+
+	# response = bew.query_bew(data['bewId'], db)
+	response = bew.query_bew_id(id)
+	return response
+
+
+# curl -X POST -H "Content-Type: application/json" -d '{"message": "ola"}' http://localhost:5000/api/echo
+# curl -X POST -H "Content-Type: application/json" -d '{"userId": "64049cc17ee9087557d9e1af", "userId2": "64049d3c7ee9087557d9e1b0", "bewId": "CUR001F03G2ETFER3000002011502", "bewId2": "SANTOCRISTO", "secret_key": "CHAVESUPERSECRETA321123"}' http://localhost:5000/api/trade_bews
+# curl -X POST -H "Content-Type: application/json" -d '{"userId": "64049d3c7ee9087557d9e1b0", "userId2": "64049cc17ee9087557d9e1af", "bewId": "CUR001F03G2ETFER3000002011502", "bewId2": "SANTOCRISTO", "secret_key": "CHAVESUPERSECRETA321123"}' http://localhost:5000/api/trade_bews
