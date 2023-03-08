3f3a10d1d885d74ae77b84795afd4459de6c87d4
Wilcker Kauã <85180287+WilckerK@users.noreply.github.com>
2023-03-07 22:51:52 -0300

    Update race.py

--- routes/admin/race.py
+++ routes/admin/race.py
@@ -5,18 +5,13 @@ from __main__ import admin
 @admin.route('/race', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        _id = f'{db.bews.count_document({}):03d}'
+        _id = f'{db.bews.count_documents({}):03d}'
         name = request.form['name']
-        rosto = request.form['rosto'].replace(" ","").split(",")
-        imagem = request.form['imagem']
+        face = request.form['rosto'].replace(" ","").split(",")
+        image = request.form['imagem']
         brasão = request.form["brasão"].replace(" ","").split(",")
-        mongo.db.race.insert_one({"_id": _id, "name":name, "rosto":rosto, "imagem":imagem, "brasão":brasão})
-        json = {
-            "name":name,
-            "rosto":rosto,
-            "imagem":imagem,
-            "brasao":brasão
-        }
-        return jsonify(json)
+        json = jsonify({_id, name, face, image,brasão})
+        mongo.db.race.insert_one(json)
+        return json
       
-    return render_template("admin.html")
+    return render_template("race.html")
