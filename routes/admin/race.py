d62c2d3032d162ca3bad3143ba1b77a6f91df2a9
Wilcker Kauã <85180287+WilckerK@users.noreply.github.com>
2023-03-07 22:24:39 -0300

    Create race.py

---
+++ routes/admin/race.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request, render_template, url_for
+from flask_pymongo import PyMongo
+
+@app.route('/race', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        _id = f'{db.bews.count_document({}):03d}'
+        name = request.form['name']
+        rosto = request.form['rosto'].replace(" ","").split(",")
+        imagem = request.form['imagem']
+        brasão = request.form["brasão"].replace(" ","").split(",")
+        mongo.db.race.insert_one({"_id": _id, "name":name, "rosto":rosto, "imagem":imagem, "brasão":brasão})
+        json = {
+            "name":name,
+            "rosto":rosto,
+            "imagem":imagem,
+            "brasao":brasão
+        }
+        return jsonify(json)
+      
+    return render_template("admin.html")
